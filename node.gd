ae6114ff5f82779049c0a0d6f7e3f412e1cf3a3e
Mike Webb <W3bbey@users.noreply.github.com>
2026-03-16 07:10:39 -0400

    Test: new line

--- node.gd
+++ node.gd
@@ -4,6 +4,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("Hello Godot")
+	print("Here's another print")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
