c5877fd96cf488e551861e285a4587846a7576b3
Mike Webb <mierwebb@gmail.com>
2026-03-24 11:25:19 -0400

    Change: map2d

--- scripts/Map/Map2d.gd
+++ scripts/Map/Map2d.gd
@@ -3,17 +3,3 @@ extends TileMapLayer
 class_name Map2d
 
 enum terrain_data_types { terrain_type }
-
-func _ready():
-	print(get_terrain_data_for_tile(0, 0, 0, terrain_data_types.terrain_type))
-	
-	
-func get_terrain_data_for_tile(layer, data, x, y):
-	var tile:TileData = get_cell_tile_data(x, y, Vector2i) # This is broken because I'm using TileMapLayer, not the deprecated TileMapp
-	
-	if (tile != null):
-		return tile.get_custom_data(terrain_data_types.keys()[data])
-	else: 
-		return null
-	
-	
