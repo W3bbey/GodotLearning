f6942cfe77217cc3ffef215d501978137eafaf61
Mike Webb <W3bbey@users.noreply.github.com>
2026-03-26 07:14:20 -0400

    Add: tilemaplayer script

--- scripts/Map/Map2d.gd
+++ scripts/Map/Map2d.gd
@@ -2,4 +2,19 @@ extends TileMapLayer
 
 class_name Map2d
 
+# enums are typically a label but associated with an integer
 enum terrain_data_types { terrain_type }
+
+func _ready():
+	print (get_terrain_data_for_tile(0, 0, 0, terrain_data_types.terrain_type))
+	
+# Data will be the terrain type specified in the custom data
+# my custom data layer is called: terrain_type
+# x and y are the cell values
+func get_terrain_data_for_tile(layer, data, x, y):
+	var tile:TileData = get_cell_tile_data(Vector2i(x,y))
+	
+	if (tile != null):
+		return tile.get_custom_data(terrain_data_types.keys()[data])
+	else:
+		return null
